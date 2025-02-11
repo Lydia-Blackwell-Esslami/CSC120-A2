cdee15186e4e1ec77b112181fef7af96c7d1a829
Lydia_Blackwell-Esslami <lblackwellesslami@smith.edu>
2025-02-10 21:31:38 -0500

    added part of the resale class and gave up without testing it

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -4,7 +4,23 @@ class ResaleShop:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, account:int, inventory:list):
+        self.funds = account
+        self.inventory = inventory
+    
+    def buy_computer(self, computer):
+        self.inventory.append(computer)
+        self.funds -= computer.price
+    
+    def sell_computer(self, computer_index: int):
+        computer = self.inventory[computer_index]
+        self.funds += computer.price
+        self.inventory.pop(computer_index)
+
+    def check_inventory(self):
+        for item in self.inventory:
+            print(item)
+
+
 
     # What methods will you need?
--- reflection.md
+++ reflection.md
@@ -1,3 +1,6 @@
 Use this file to record your reflection on this assignment. 
 
 What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+I don't get why we have a separate file for the computer class and the ship class. In my opinion the entire thing should be done in one file so there's no need to have to use an import statement to make it work, which creates unnessecary complications. Also the instructions were not really clear, probably because the day the assignment was supposed to be explained class was canceled due to the snow. Additionally due to life circumstances I didn't really have time to work on this properly and by the time I did start working it I didn't really have any motivation or interest in it, so I have not done a very good job here. 
